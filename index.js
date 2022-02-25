cdb0957a0df82d318a0eae2388a4f89ce7394508
Kelvin Lopes <kelvinbrunolopes@gmail.com>
2022-02-25 12:33:27 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,111 +1,58 @@
-// function snapCrackle(maxValue){
-//     let a = ''
-//     for(let i = 1; i <= maxValue; i++){
-//         if(i % 2 != 0){
-//         a += ' Snap,'
-//     }
-//         if(i % 5 == 0){
-//         a += ' Crackle,'
-//     }
-//         if(i % 2 != 0 && i % 5 == 0){
-//         a += ' SnapCrackle,'
-//     }
-//         if(i % 2 == 0 && i % 5 != 0){
-//         a += ' '+ i + ','
-//     }
-//     }
-//     return a
-// }
-// console.log(snapCrackle(12))
+function snapCrackle(valorMaximo) {
+  let frase = "";
+  for (let i = 1; i <= valorMaximo; i++) {
+    if (i % 2 != 0 && i % 5 != 0) {
+      frase += " Snap,";
+    }
+    if (i % 5 == 0 && i % 2 == 0) {
+      frase += " Crackle,";
+    }
+    if (i % 2 != 0 && i % 5 == 0) {
+      frase += " SnapCrackle,";
+    }
+    if (i % 2 == 0 && i % 5 != 0) {
+      frase += " " + i + ",";
+    }
+  }
+  return frase;
+}
+function primos(a) {
+  for (let i = 2; i < a; i++){
+    if (a % i === 0) {
+      return false;
+    }}
+  return a > 1;
+}
 
-// function snapCracklePrime(maxValue){
-//     let a = ''
-//     for(let i = 1; i <= maxValue; i++){
-        
-//         if(i % 2 != 0){
-//         a += ' Snap,'
-//     }
-//         if(i== 2 || (i%2!=0 && i%3!=0 && i%5!=0)){
-//         a += ' Prime,'
-//         }
-//         if(i % 5 == 0){
-//         a += ' Crackle,'
-//     }
-//         if(i % 2 != 0 && i % 5 == 0 && i%3!= 0){
-//         a += ' SnapCracklePrime,'
-//     }
-//         if(i % 2 != 0 && i % 5 == 0 && i%3==0){
-//         a += ' SnapCrackle,'
-//     }
-//         if(i % 2 == 0 && i % 5 != 0 && i%3!=0){
-        
-//         a += ' '+ i + ','
-//     }
-//     }
-//     return a
-// }
-function isPrime(a){  
-    for (let i = 2; i < a; i++)
-      if (a % i === 0) {
-        return false;
+function snapCracklePrime(valorMaximo) {
+  let primo = null;
+  let frase = "";
+  for (let i = 1; i <= valorMaximo; i++) {
+    primo = primos(i);
+    if (i % 2 != 0 && i % 5 != 0) {
+      if (primo == true) {
+        frase += " SnapPrime,";
+      } else {
+        frase += " Snap,";
       }
-    return a > 1;
-  };
-  
-  function snapCrackle(maxValue){
-      let result = ''
-      for(let i = 1; i <= maxValue; i++){
-          if(i % 2 != 0 && i % 5 != 0){
-          result += ' Snap,'
+    }
+    if (i % 5 == 0 && i % 2 == 0) {
+      frase += " Crackle,";
+    }
+    if (i % 2 != 0 && i % 5 == 0) {
+      if (primo == true) {
+        frase += " SnapCracklePrime,";
+      } else {
+        frase += " SnapCrackle,";
       }
-          if(i % 5 == 0 && i%2==0){
-          result += ' Crackle,'
+    }
+    if (i % 2 == 0 && i % 5 != 0) {
+      if (primo == true) {
+        frase += " Prime,";
+      } else {
+        frase += " " + i + ",";
       }
-          if(i % 2 != 0 && i % 5 == 0){
-          result += ' SnapCrackle,'
-      }
-          if(i % 2 == 0 && i % 5 != 0){
-          result += ' '+ i + ','
-      }
-      }
-      return result
+    }
   }
-  function snapCracklePrime(maxValue){
-      let primo = null;
-      let result = ''
-      for(let i = 1; i <= maxValue; i++){
-         primo = isPrime(i)
-          if(i % 2 != 0 && i % 5 != 0){
-          if(primo == true){
-            result += ' SnapPrime,'
-          }
-          else{
-          result += ' Snap,'
-        }
-      }
-          if(i % 5 == 0 && i%2==0){
-          result += ' Crackle,'
-        }
-          if(i % 2 != 0 && i % 5 == 0){
-             if(primo == true){
-            result += ' SnapCracklePrime,'
-          }
-          else{
-          result += ' SnapCrackle,'
-        }
-          
-      }
-          if(i % 2 == 0 && i % 5 != 0){
-            if(primo == true){
-            result += ' Prime,'
-          }
-          else{
-          result += ' '+ i + ','
-        }
-          
-          
-      }
-      }
-      return result
-  }
-  
+  return frase;
+}
